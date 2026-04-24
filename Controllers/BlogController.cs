61e23d53017bdcf96f0a12351f8c35850c5d0db3
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2026-04-24 11:36:47 +0600

    get method done

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -1,6 +1,8 @@
 ﻿using Dayli_Blog.Data;
+using Dayli_Blog.Models.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dayli_Blog.Controllers
 {
@@ -16,6 +18,48 @@ namespace Dayli_Blog.Controllers
         {
             this.dbContext = dbContext;
             this.environment = environment;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBlogs()
+        {
+            var blogs = await dbContext.Blogs
+                .OrderByDescending(b => b.CreatedAt)
+                .Select(b => new BlogResponseDto
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Description = b.Description,
+                    Location = b.Location,
+                    ImagePath = b.ImagePath,
+                    CreatedAt = b.CreatedAt,
+                    UpdatedAt = b.UpdatedAt
+                })
+                .ToListAsync();
+
+            return Ok(blogs);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetBlogById(Guid id)
+        {
+            var blog = await dbContext.Blogs.FindAsync(id);
+
+            if (blog is null)
+                return NotFound("Blog is not found!");
+
+            var response = new BlogResponseDto
+            {
+                Id = blog.Id,
+                Title = blog.Title,
+                Description = blog.Description,
+                Location = blog.Location,
+                ImagePath = blog.ImagePath,
+                CreatedAt = blog.CreatedAt,
+                UpdatedAt = blog.UpdatedAt
+            };
+
+            return Ok(response);
         }
     }
 }
