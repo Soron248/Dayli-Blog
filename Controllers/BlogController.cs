7693347e0aaee1f3f60b80c613d516f52012a269
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2026-04-24 10:57:36 +0600

    controller created

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Dayli_Blog.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dayli_Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext; 
+        private readonly IWebHostEnvironment environment; // for image
+
+
+        public BlogController(ApplicationDbContext dbContext, IWebHostEnvironment environment)
+        {
+            this.dbContext = dbContext;
+            this.environment = environment;
+        }
+    }
+}
